4d6185cff666139d3ca38b661b6c23a2defce332
ewangso <skyromex@gmail.com>
2021-09-23 19:38:17 -0400

    First pages commit

--- src/Project.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import projectData from './projectData';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-
-function Project(){
-    return(<p></p>);
-}
-
-export default Project;

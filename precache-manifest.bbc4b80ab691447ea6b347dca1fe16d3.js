6b4fccc99f2332b3c4bf05d1537063d189ef7379
ewangso <skyromex@gmail.com>
2021-09-23 19:48:30 -0400

    Updates

---
+++ precache-manifest.bbc4b80ab691447ea6b347dca1fe16d3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5942593e18fb1aa5103f83ec1e6ca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522e61f04e3e105410b0",
+    "url": "/static/css/main.04ec7b99.chunk.css"
+  },
+  {
+    "revision": "e46ca3f94ffea8e3f976",
+    "url": "/static/js/2.a4dd7098.chunk.js"
+  },
+  {
+    "revision": "b744062dae966d6e3f967726c85e050b",
+    "url": "/static/js/2.a4dd7098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522e61f04e3e105410b0",
+    "url": "/static/js/main.8d54ad74.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "648c029de1e0dc9968deeeef959da7c9",
+    "url": "/static/media/21_Resume.648c029d.pdf"
+  },
+  {
+    "revision": "a450be35298f3cb745fa332afce08169",
+    "url": "/static/media/CS2020RESUME.a450be35.pdf"
+  },
+  {
+    "revision": "28fd4fb07a70f49e220624e89f963ba1",
+    "url": "/static/media/MedOntology.28fd4fb0.png"
+  },
+  {
+    "revision": "14228c7a6cea369958dfb2708b4624dc",
+    "url": "/static/media/Twitter-Logo.14228c7a.png"
+  },
+  {
+    "revision": "5b168f428a275192dc3c221277a5ab9d",
+    "url": "/static/media/coding.5b168f42.jpeg"
+  },
+  {
+    "revision": "d8cd809c520896eba4cc45eca0c9c9cc",
+    "url": "/static/media/computerdesk.d8cd809c.jpg"
+  },
+  {
+    "revision": "e4700f800a509a5cb70358ce98044b85",
+    "url": "/static/media/facebook.e4700f80.png"
+  },
+  {
+    "revision": "c5c3769f8bc301a1041dcf4f26242c26",
+    "url": "/static/media/flashcards.c5c3769f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "35cd9cd4450537aa73160cb44d66bea0",
+    "url": "/static/media/map.35cd9cd4.jpg"
+  },
+  {
+    "revision": "36743b18c4b8a8c7ea1c5a3d004e1f38",
+    "url": "/static/media/njit.36743b18.png"
+  }
+]);
